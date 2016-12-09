cd2bf724e4462bed539fd4b23ec924dc54acf178
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2016-12-09 16:47:48 +0100

    Initial commit

---
+++ DistValetDriver.php
@@ -0,0 +1,45 @@
+<?php
+
+class VueValetDriver extends ValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return bool
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        return file_exists($sitePath . '/dist/index.html');
+    }
+
+    /**
+     * Determine if the incoming request is for a static file.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string|false
+     */
+    public function isStaticFile($sitePath, $siteName, $uri)
+    {
+        return $sitePath . '/dist' . $uri;
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string
+     */
+    public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+        return (explode('/', $uri)[1] !== 'static')
+            ? $sitePath . '/dist/index.html'
+            : $sitePath . '/dist' . $uri;
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Laravel Valet /dist/ Driver
+Custom `/dist/` driver for [Laravel Valet](https://laravel.com/docs/master/valet).
+
+## What is this?
+This is a custom driver to handle any site using `/dist/index.html` as their front controller.
+
+Compatible with Vue, Webpack and many more.
+
+## Installation
+1. `git clone https://github.com/DCzajkowski/DistValetDriver.git ~/.valet/Drivers`.
+2. Done :)
